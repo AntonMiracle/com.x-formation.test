43eaedcc1214d75942ce7bdbb26cd8e7812a9b52
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-25 10:15:16 +0200

    update domain.model.Meal

--- src/main/java/com/xformation/restaurant/domain/model/Meal.java
+++ src/main/java/com/xformation/restaurant/domain/model/Meal.java
@@ -1,12 +1,8 @@
 package com.xformation.restaurant.domain.model;
 
-import java.math.BigDecimal;
 import java.util.Map;
 
-public interface Meal {
-    BigDecimal getPrice();
-
-    String getName();
+public interface Meal extends MenuUnit {
 
     Cuisine getCuisine();
 

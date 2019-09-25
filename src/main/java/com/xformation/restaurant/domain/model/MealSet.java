5e3a7315de122e0e22caaea68efd48e97e8a71e1
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-25 10:36:42 +0200

    update domain.model.MealSet, domain.model.impl.Lunch

--- src/main/java/com/xformation/restaurant/domain/model/MealSet.java
+++ src/main/java/com/xformation/restaurant/domain/model/MealSet.java
@@ -2,6 +2,6 @@ package com.xformation.restaurant.domain.model;
 
 import java.util.Map;
 
-public interface MealSet {
+public interface MealSet extends MenuUnit{
     Map<Meal,Integer> getMeals();
 }
--- src/main/java/com/xformation/restaurant/domain/model/impl/Lunch.java
+++ src/main/java/com/xformation/restaurant/domain/model/impl/Lunch.java
@@ -3,12 +3,17 @@ package com.xformation.restaurant.domain.model.impl;
 import com.xformation.restaurant.domain.model.Meal;
 import com.xformation.restaurant.domain.model.MealSet;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class Lunch implements MealSet {
+    private BigDecimal price;
+    private String name;
     private Map<Meal, Integer> meals;
 
-    public Lunch(Map<Meal, Integer> meals) {
+    public Lunch(BigDecimal price, String name, Map<Meal, Integer> meals) {
+        this.price = price;
+        this.name = name;
         this.meals = meals;
     }
 
@@ -19,16 +24,31 @@ public class Lunch implements MealSet {
 
         Lunch lunch = (Lunch) o;
 
+        if (price != null ? !price.equals(lunch.price) : lunch.price != null) return false;
+        if (name != null ? !name.equals(lunch.name) : lunch.name != null) return false;
         return meals != null ? meals.equals(lunch.meals) : lunch.meals == null;
     }
 
     @Override
     public int hashCode() {
-        return meals != null ? meals.hashCode() : 0;
+        int result = price != null ? price.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (meals != null ? meals.hashCode() : 0);
+        return result;
     }
 
     @Override
     public Map<Meal, Integer> getMeals() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
         return null;
     }
 }

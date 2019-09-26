15a3849db8edbf136fc9d6edd8f597a3be95586c
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 08:50:11 +0200

    update validator.ModelValidator

--- src/main/java/com/xformation/restaurant/domain/validator/ModelValidator.java
+++ src/main/java/com/xformation/restaurant/domain/validator/ModelValidator.java
@@ -4,9 +4,6 @@ import com.xformation.restaurant.domain.model.MenuUnit;
 
 public interface ModelValidator {
     default boolean isMenuUnitNameValid(MenuUnit menuUnit) {
-        if (menuUnit != null && menuUnit.getName() != null) {
-            return menuUnit.getName().length() > 0;
-        }
-        return false;
+        return menuUnit.getName() != null && menuUnit.getName().length() > 0;
     }
 }

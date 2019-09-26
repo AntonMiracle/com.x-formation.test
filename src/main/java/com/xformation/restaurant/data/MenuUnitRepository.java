f310e57d1f7672254a7c5f3a05381226285d1c44
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 08:19:31 +0200

    update data.MenuUnitRepository

--- src/main/java/com/xformation/restaurant/data/MenuUnitRepository.java
+++ src/main/java/com/xformation/restaurant/data/MenuUnitRepository.java
@@ -2,14 +2,32 @@ package com.xformation.restaurant.data;
 
 import com.xformation.restaurant.domain.model.MenuUnit;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public interface MenuUnitRepository {
-    void save(MenuUnit menuUnit);
+    Set<MenuUnit> repository = new HashSet<>();
 
-    MenuUnit findByName(String name);
+    default void save(MenuUnit menuUnit) {
+        repository.add(menuUnit);
+    }
 
-    void remove(MenuUnit menuUnit);
+    default MenuUnit findByName(String name) {
+        for (MenuUnit unit : repository) {
+            if (unit.getName().equals(name)) {
+                return unit;
+            }
+        }
+        return null;
+    }
 
-    List<MenuUnit> findAll();
+    default void remove(MenuUnit menuUnit) {
+        repository.remove(menuUnit);
+    }
+
+    default List<MenuUnit> findAll() {
+        return new ArrayList<>(repository);
+    }
 }

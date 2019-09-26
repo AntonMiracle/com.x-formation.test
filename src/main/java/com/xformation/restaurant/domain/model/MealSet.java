43b5b52f3672270c65e2e7c1a2eb95af9724857a
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 11:32:42 +0200

    update domain.model.MealSet

--- src/main/java/com/xformation/restaurant/domain/model/MealSet.java
+++ src/main/java/com/xformation/restaurant/domain/model/MealSet.java
@@ -3,5 +3,5 @@ package com.xformation.restaurant.domain.model;
 import java.util.Map;
 
 public interface MealSet extends MenuUnit{
-    Map<Meal,Integer> getMeals();
+    Map<MenuUnit,Integer> getMeals();
 }

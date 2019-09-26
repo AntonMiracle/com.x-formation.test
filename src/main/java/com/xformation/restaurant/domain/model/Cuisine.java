a04dfb288c471af90bf87a0c1c1e3ff1f2a6731e
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 10:20:59 +0200

    update domain.model.Cuisine

--- src/main/java/com/xformation/restaurant/domain/model/Cuisine.java
+++ src/main/java/com/xformation/restaurant/domain/model/Cuisine.java
@@ -1,5 +1,5 @@
 package com.xformation.restaurant.domain.model;
 
 public enum Cuisine {
-    POLISH, ITALY, MEXICAN
+    POLISH, ITALY, MEXICAN, POLISH_ITALY_MEXICO, POLISH_ITALY, POLISH_MEXICO, ITALY_MEXICO;
 }

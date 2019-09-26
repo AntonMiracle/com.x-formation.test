fe581077e6705779e98b079f58433533fe23b636
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 10:25:54 +0200

    update domain.service.MenuUnitService

--- src/main/java/com/xformation/restaurant/domain/service/MenuUnitService.java
+++ src/main/java/com/xformation/restaurant/domain/service/MenuUnitService.java
@@ -2,18 +2,17 @@ package com.xformation.restaurant.domain.service;
 
 import com.xformation.restaurant.domain.model.MenuUnit;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface MenuUnitService<T extends MenuUnit> {
-    Optional<T> add(String menuUnitName, BigDecimal menuUnitPrice);
+    Optional<T> add(T menuUnit);
 
-    boolean remove(MenuUnit menuUnitToRemove);
+    boolean remove(T menuUnitToRemove);
 
     Optional<T> findByName(String name);
 
-    boolean isUnique(MenuUnit menuUnitToCheck);
+    boolean isUnique(T menuUnitToCheck);
 
     List<T> findAll();
 

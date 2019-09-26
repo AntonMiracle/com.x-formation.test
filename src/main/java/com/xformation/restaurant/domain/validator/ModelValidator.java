f410184c37483d55f5f9b34d6ce7b3d9d0780137
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-09-26 09:30:09 +0200

    update domain.validator.ModelValidator

--- src/main/java/com/xformation/restaurant/domain/validator/ModelValidator.java
+++ src/main/java/com/xformation/restaurant/domain/validator/ModelValidator.java
@@ -6,7 +6,12 @@ public interface ModelValidator {
     default boolean isMenuUnitNameValid(String name) {
         return name != null && name.length() > 0;
     }
+
     default boolean isPriceValueValid(BigDecimal price) {
         return price != null && BigDecimal.ZERO.compareTo(price) <= 0;
+    }
+
+    default boolean isOrderIdValid(Long id) {
+        return Long.valueOf(0).compareTo(id) < 0;
     }
 }
